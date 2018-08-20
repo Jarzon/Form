948ddec81a4437612d16475eb5ecb9ca6ea7075d
Jarzon <j@jasonvaillancourt.ca>
2018-08-20 00:24:11 -0400

    Add ability to get select value

--- src/Input/SelectInput.php
+++ src/Input/SelectInput.php
@@ -5,12 +5,20 @@ use Jarzon\ListBasedInput;
 
 class SelectInput extends ListBasedInput
 {
-    protected $values = [];
-
     public function __construct(string $name)
     {
         parent::__construct($name);
         $this->setTag('select');
+    }
+
+    public function __get($name)
+    {
+        $return = parent::__get($name);
+        if($return) {
+            return $return;
+        } else if($name === 'values') {
+            return $this->values;
+        }
     }
 
     public function generateHtml()
