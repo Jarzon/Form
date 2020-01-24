10afc9191dc6dd89d74cc92ee318ff50b8fb6347
Jarzon <j@jasonvaillancourt.ca>
2020-01-24 17:37:23 -0500

    Save past CSRF token before overwriting it

--- src/Input/CsrfInput.php
+++ src/Input/CsrfInput.php
@@ -6,6 +6,7 @@ use Jarzon\TextBasedInput;
 class CsrfInput extends TextBasedInput
 {
     public string $token;
+    public string $pastToken;
 
     public function __construct(string $name, $form)
     {
@@ -15,6 +16,12 @@ class CsrfInput extends TextBasedInput
         $this->token = bin2hex(random_bytes(10));
 
         $this->value($this->token);
+        $this->required();
+
+        if(isset($_SESSION['_formToken'])) {
+            $this->pastToken = $_SESSION['_formToken'];
+        }
+
         $_SESSION['_formToken'] = $this->token;
     }
 
@@ -22,7 +29,7 @@ class CsrfInput extends TextBasedInput
     {
         parent::passValidation($value);
 
-        if($value !== $_SESSION['_formToken']) {
+        if($value !== $this->pastToken) {
             throw new \Jarzon\ValidationException("the CSRF token doesn't match");
         }
 
