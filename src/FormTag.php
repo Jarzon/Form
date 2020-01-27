6ce0f3fc4412b18e250788a880adabb3c491d693
Jarzon <j@jasonvaillancourt.ca>
2020-01-27 18:48:49 -0500

    Fix missing ability to chain on formtag

--- src/FormTag.php
+++ src/FormTag.php
@@ -16,23 +16,31 @@ class FormTag extends Tag
         $this->setTag($tag);
     }
 
-    public function id(string $id)
+    public function id(string $id): FormTag
     {
         $this->setAttribute('id', $id);
+
+        return $this;
     }
 
-    public function method(string $method)
+    public function method(string $method): FormTag
     {
         $this->setAttribute('method', $method);
+
+        return $this;
     }
 
-    public function action(string $url)
+    public function action(string $url): FormTag
     {
         $this->setAttribute('action', $url);
+
+        return $this;
     }
 
-    public function target(string $target)
+    public function target(string $target): FormTag
     {
         $this->setAttribute('target', $target);
+
+        return $this;
     }
 }
