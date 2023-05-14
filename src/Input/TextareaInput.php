66b9104dd1361733ec959228c15decdf2c47b99e
Jarzon <jason@libellum.ca>
2023-05-14 07:35:59 -0400

    Fix textarea max php validation not matching html max since it doesnt count the line returns

--- src/Input/TextareaInput.php
+++ src/Input/TextareaInput.php
@@ -3,6 +3,7 @@ namespace Jarzon\Input;
 
 use Jarzon\Input;
 use Jarzon\TextBasedInput;
+use Jarzon\ValidationException;
 
 class TextareaInput extends TextBasedInput
 {
@@ -22,5 +23,25 @@ class TextareaInput extends TextBasedInput
     public function generateHtml(): void
     {
         $this->setHtml($this->generateTag($this->tag, $this->attributes, $this->value));
+    }
+
+    public function passValidation($value = ''): bool
+    {
+        if($value == '' && $this->isRequired) {
+            throw new ValidationException("{$this->name} is required", 1);
+        }
+
+        $value = (string)$value;
+
+        $numberChars = mb_strlen($value);
+        $lineBreaks = $this->max > 0? mb_substr_count($value, "\n") : 0;
+        if(!empty($this->max) && (($numberChars - $lineBreaks) > $this->max || $lineBreaks > $this->max)) {
+            throw new ValidationException("{$this->name} is too long", 20);
+        }
+        else if(!empty($this->min) && ($numberChars - $lineBreaks) < $this->min) {
+            throw new ValidationException("{$this->name} is too short", 21);
+        }
+
+        return $value !== '';
     }
 }
